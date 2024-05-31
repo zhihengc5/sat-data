2fc40876dba627a4ee332b285bc7569a1c180f6f
jhans-404 <72055876+jhans-404@users.noreply.github.com>
2024-05-31 08:46:04 -0400

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("SAT DATA!!!!");
+
+    // create data structure to store all total scores
+    ArrayList<Integer> totalScore = new ArrayList<Integer>();
+    
+    try{
+      Scanner s = new Scanner(new File("src/main/total.txt"));
+
+      while(s.hasNext()){
+        int score = Integer.parseInt(s.nextLine());
+        totalScore.add(score);
+      }
+      s.close();
+    }
+
+    catch(Exception e){
+      System.out.println("file not found");
+    }
+
+    // make sure all data entered the ArrayList
+    System.out.println(totalScore);
+    System.out.println(totalScore.size());
+    
+  }
+
+}
