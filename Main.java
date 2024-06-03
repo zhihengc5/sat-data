be699971d6f5f0cb95a6de4768241c7893bb18ef
zhihengc5 <zhihengc5@nycstudents.net>
2024-06-03 15:43:36 +0000

    update

--- Main.java
+++ Main.java
@@ -20,12 +20,40 @@ public class Main {
 
     catch(Exception e){
       System.out.println("file not found");
+      
+    }
+    ArrayList<Integer> readingtotal= new ArrayList<Integer>();
+    try{
+      Scanner s = new Scanner(new File("reading.txt"));
+
+      while(s.hasNext()){
+        int score = Integer.parseInt(s.nextLine());
+        readingtotal.add(score);
+      }
+      s.close();
+    }
+
+    catch(Exception e){
+      System.out.println("file not found");
     }
 
     // make sure all data entered the ArrayList
     //System.out.println(totalScore);
     System.out.println(totalScore.size()+" Students took the test.");
-    
+
+
+    int total=0;
+    for(int score: totalScore){
+      total+=score;
+    }
+    double average=(double)total/totalScore.size();
+    System.out.println(average);
+
+    int total2=0;
+    for(int read: readingtotal){
+      total2+=read;
+    }
+    System.out.println((double)total2/readingtotal.size());
   }
 
 }
